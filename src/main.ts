426dd1fef0946b29192b3918fbfbd1a20e35d791
ousttrue <ousttrue@gmail.com>
2019-05-26 15:51:49 +0900

    gl.clear

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,27 @@
-console.log('hello')
+function main() {
+  const canvas = <HTMLCanvasElement>document.querySelector("#glCanvas");
+  if (canvas === null) {
+    console.log("no canvas");
+    return;
+  }
+  console.log(canvas);
+
+  // Initialize the GL context
+  const gl = canvas.getContext("webgl");
+  console.log(gl);
+
+  // Only continue if WebGL is available and working
+  if (gl === null) {
+    console.error(
+      "Unable to initialize WebGL. Your browser or machine may not support it."
+    );
+    return;
+  }
+
+  // Set clear color to black, fully opaque
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  // Clear the color buffer with specified clear color
+  gl.clear(gl.COLOR_BUFFER_BIT);
+}
+
+main();

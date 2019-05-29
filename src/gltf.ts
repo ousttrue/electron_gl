9a6f56bad8b6a799f5e3323632f383bdd4719cf9
ousttrue <ousttrue@gmail.com>
2019-05-30 05:45:36 +0900

    add tests

--- src/gltf.ts
+++ src/gltf.ts
@@ -1,13 +1,39 @@
 // https://github.com/KhronosGroup/glTF/blob/master/specification/2.0/README.md
 
+export interface Asset {
+    version: string; // "2.0"
+    generator?: string;
+    copyright?: string;
+}
+
 export interface Primitive {
-    attributes: {}
+    attributes: { [key: string]: number };
 }
 
 export interface Mesh {
     primitives: Primitive[]
 }
 
+export enum ComponentType {
+    UNSIGNED_SHORT = 5123,
+}
+
+export enum ValueType {
+    SCALAR = 'SCALAR',
+}
+
+export interface Accessor {
+    bufferView: number,
+    byteOffset: number,
+    componentType: ComponentType,
+    count: number,
+    max: number[],
+    min: number[],
+    type: ValueType
+}
+
 export interface Gltf {
+    asset: Asset;
     meshes: Mesh[];
+    accessors: Accessor[];
 }
--- tests/glb_test.ts
+++ tests/glb_test.ts
@@ -1,8 +1,11 @@
 import { describe, it } from "mocha";
-import * as glb from "../src/glb";
 import { assert } from "chai";
 import * as path from "path";
 import * as fs from "fs";
+import { AssertionError } from "assert";
+import { TextDecoder } from 'util';
+import * as glb from "../src/glb";
+import * as gltf from "../src/gltf";
 
 describe('glb parser', () => {
     it('parse duck', () => {
@@ -12,5 +15,20 @@ describe('glb parser', () => {
 
         assert.instanceOf(gltf_bin, Uint8Array);
         assert.instanceOf(bin, Uint8Array);
+
+        const gltf_json = new TextDecoder('utf-8').decode(gltf_bin);
+        const gltf_value = <gltf.Gltf>JSON.parse(gltf_json);
+        assert.equal(gltf_value.asset.version, "2.0");
+        assert.equal(gltf_value.meshes.length,  1);
+
+        const mesh = gltf_value.meshes[0];
+        assert.equal(mesh.primitives.length, 1);
+
+        const primitive = mesh.primitives[0];
+        assert.equal(gltf_value.accessors.length, 4); // pos, normal, uv + index
+
+        const index_accessor = gltf_value.accessors[0];
+        assert.equal(index_accessor.componentType, gltf.ComponentType.UNSIGNED_SHORT);
+        assert.equal(index_accessor.type, gltf.ValueType.SCALAR);
     });
 })
